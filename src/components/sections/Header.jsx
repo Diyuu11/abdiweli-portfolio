78868dbbc8c1743c0486529ad7eb288556768d0f
hamdiabdiaden <diagodu198@gmail.com>
2025-05-23 10:36:10 +0300

    first commit

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="text-center my-4 pt-5">
-      <img src="../src/assets/profile.png" alt="Abdiweli" className="rounded-circle" width="120" height="120" />
+      <img src="../profile.png" alt="Abdiweli" className="rounded-circle" width="120" height="120" />
       <h1 className="display-4 fw-bold mt-3">Abdiweli Mohamed</h1>
       <p className="lead">Project Management | Policy & Governance | Gender & Development</p>
       <div className="d-flex justify-content-center gap-3">
